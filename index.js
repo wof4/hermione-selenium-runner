253c378ae775da46a4e9245a8141c866cf9eca8c
a.adamov <wof_4@mail.ru>
2021-07-08 13:20:10 +0300

    add

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-
 const fsp = require('fs/promises');
 const delay = require('delay');
 const { spawn } = require('child_process');
 
-const { spawn } = require('child_process');
-const delay = require('delay');
-
-module.export =  (hermione) => {
+module.exports = (hermione) => {
     let selenium;
+
     hermione.on(hermione.events.RUNNER_START, async () => {
-        const file = fs.openSync('selenium.log', 'w');
+        const file = await fsp.open('selenium.log', 'w');
 
         selenium = spawn('selenium-standalone', ['start'], {
-            stdio: ['ignore', file, file]
-        })
+            stdio: ['ignore', file, file],
+        });
+
         await delay(2000);
     });
 
-
-    hermione.on(hermione.events.RUNNER_START, () => {
+    hermione.on(hermione.events.RUNNER_END, () => {
         return new Promise((resolve) => {
-            selenium.on('exit', () => resolve())
+            selenium.on('exit', () => resolve());
 
             selenium.kill();
         });
-    })
-}
+    });
+};
