aa80cec00677f8ce958d04c2accbcfc7bb6d9761
a.adamov <wof_4@mail.ru>
2021-07-08 12:47:59 +0300

    add runner

---
+++ index.js
@@ -0,0 +1,26 @@
+
+import * as fs from 'fs';
+
+const { spawn } = require('child_process');
+const delay = require('delay');
+
+module.export =  (hermione) => {
+    let selenium;
+    hermione.on(hermione.events.RUNNER_START, async () => {
+        const file = fs.openSync('selenium.log', 'w');
+
+        selenium = spawn('selenium-standalone', ['start'], {
+            stdio: ['ignore', file, file]
+        })
+        await delay(2000);
+    });
+
+
+    hermione.on(hermione.events.RUNNER_START, () => {
+        return new Promise((resolve) => {
+            selenium.on('exit', () => resolve())
+
+            selenium.kill();
+        });
+    })
+}
